01f7230d92a666469fac8c1b4391f02b74fa680c
msf <msfwolski@gmail.com>
2018-05-03 12:11:58 +0200

    Troche drobnych zmian

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //////////////////////////////////////
 #include <stdio.h>
 #include <iostream> 
+
 #include <GL/gl3w.h>   
 #include <GLFW/glfw3.h> 
 #include <glm.hpp>
@@ -34,6 +35,9 @@ int main(int, char**)
     glfwSwapInterval(1); // Enable vsync
     gl3wInit();
 
+
+	
+
     // Setup ImGui binding
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io; 
@@ -43,61 +47,66 @@ int main(int, char**)
     ImGui::StyleColorsDark(); 
 
     bool show_demo_window = false; 
+	bool show_ca_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f); 
 	 
 
-
-	GLuint tex;
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	// Black/white checkerboard
-	float pixels[] = {
-		0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 1.0f,
-		1.0f, 0.0f, 1.0f,   0.0f, 1.0f, 0.0f
-	};
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-	float color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
-	glGenerateMipmap(GL_TEXTURE_2D);
-
     // Main loop
-    while (!glfwWindowShouldClose(window))
-    { 
-        glfwPollEvents();
-        ImGui_ImplGlfwGL3_NewFrame();
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		ImGui_ImplGlfwGL3_NewFrame();
 
-        // 1. Show a simple window. Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+		// 1. Show a simple window. Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
+		{
+			ImGui::Text("CA simulation 152017");                           // Display some text (you can use a format string too)
+			// ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state 
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state 
+			ImGui::Checkbox("Simulation  Window", &show_ca_window);
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+			 
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		if (show_demo_window)
+		{
+			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+			ImGui::ShowDemoWindow(&show_demo_window);
+		}
 
-			ImGui::Image(reinterpret_cast<void*>(tex), ImVec2(500.f, 500.f));
-        }  
+		if (show_ca_window)
+		{
+			ImGui::SetNextWindowPos(ImVec2(150, 200), ImGuiCond_FirstUseEver);
+			if (ImGui::Begin("Simulation Window", &show_ca_window)) // Do not generate texture unless window is open
+			{
+				static GLuint tex;
+				glGenTextures(1, &tex);
+				glBindTexture(GL_TEXTURE_2D, tex);
+				// Black/white checkerboard
+				float pixels[] = {
+					0.0f, 0.0f, 1.0f,   
+					1.0f, 1.0f, 1.0f,
+					1.0f, 0.0f, 1.0f,   
+					0.0f, 1.0f, 0.0f
+				};
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
-        if (show_demo_window)
-        {
-            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-            ImGui::ShowDemoWindow(&show_demo_window);
-        }
+				float color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+				glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+				glGenerateMipmap(GL_TEXTURE_2D);
+				ImGui::Image(reinterpret_cast<void*>(tex), ImVec2(500.f, 500.f));
+				ImGui::End();
+			}
+	
+		}
 
         // Rendering
         int display_w, display_h;
