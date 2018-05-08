ebb9e33e41b770227afe0a25e733286d977bbd53
msf <msfwolski@gmail.com>
2018-05-09 00:09:57 +0200

    Cleanup of main.cpp, moving application logic to separate layer

--- MapGenUI.h
+++ MapGenUI.h
@@ -5,19 +5,26 @@
 #include "imgui\imgui.h"
 #include "imgui\imgui_impl_glfw_gl3.h"  
 
+#include "GridTexture.h"
+#include "CellGrid.h"
+
+
+
 const char *build_str = "Build date: " __DATE__ " " __TIME__;
 
 class MapGenUI
 {
 private:
 	GLFWwindow* chosenWindow;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	GridTexture *gridWindowTex;
+
 	bool show_demo_window = false;
 	bool show_ca_window = true;
+	bool my_pixel = true; // remove this later
 	
 public:
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	bool my_pixel = true; // remove this later
-
 	MapGenUI(GLFWwindow* window)
 	{
 		chosenWindow = window;
@@ -26,6 +33,7 @@ public:
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		ImGui_ImplGlfwGL3_Init(chosenWindow, true);
 		ImGui::StyleColorsDark();
+		gridWindowTex = new GridTexture(25, 25);
 	}
 
 	~MapGenUI()
@@ -33,6 +41,14 @@ public:
 		// Cleanup
 		ImGui_ImplGlfwGL3_Shutdown();
 		ImGui::DestroyContext();
+	}
+
+	void RebindTexture()
+	{
+		 
+
+		CellGrid* a = new CellGrid(4, 4);
+		CellGrid* b = a->NextStep();
 	}
 
 	void Update() {
@@ -47,6 +63,8 @@ public:
 			std::string text = "Cell (2,3) is ";
 			text += std::to_string(my_pixel);
 			ImGui::Checkbox(text.c_str(), &my_pixel);
+			if (my_pixel) gridWindowTex->SetTexelColor(2, 3, GT_RED); 
+			else gridWindowTex->SetTexelColor(2, 3, GT_GREEN);
 		}
 
 		if (show_demo_window)
@@ -54,20 +72,18 @@ public:
 			ImGui::SetNextWindowPos(ImVec2(10, 200), ImGuiCond_FirstUseEver);
 			ImGui::ShowDemoWindow(&show_demo_window);
 		}
-	}
 
-	void UpdateCellsWindow(ImTextureID image) {
 		if (show_ca_window)
 		{
 			ImGui::SetNextWindowPos(ImVec2(600, 20), ImGuiCond_FirstUseEver);
 			if (ImGui::Begin("Generation Window", &show_ca_window,
 				ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings))
-			{
-				ImGui::Image(image, ImVec2(800.f, 800.f));
+			{ 
+				ImGui::Image(reinterpret_cast<void*>(gridWindowTex->Update()), ImVec2(800.f, 800.f));
 				ImGui::End();
 			}
 		}
-	}
+	} 
 
 	void Render() {
 		// Rendering
--- main.cpp
+++ main.cpp
@@ -4,69 +4,48 @@
 #include <GL\gl3w.h>   
 #include <GLFW\glfw3.h> 
 
-#include "MapGenUI.h"
-#include "GridTexture.h"
-#include "CellGrid.h"
+#include "MapGenUI.h" 
 
 GLFWwindow* window;
 
-
- 
 void glfw_error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Error %d: %s\n", error, description);
+	std::cerr << "GLFW Error " << error << ": " << description << std::endl;
 }
 
-unsigned int glfw_setup()
+bool glfwSetupWindow(unsigned int width, unsigned int height, const char* title)
 {
-	// Setup window
 	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit())
+	if (glfwInit())
 	{
-		return 1;
-	}
-	else {
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if __APPLE__
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-		window = glfwCreateWindow(1600, 900, "Cellular Automata Map Generator 152017", NULL, NULL);
+		window = glfwCreateWindow(width, width, title, NULL, NULL);
 		glfwMakeContextCurrent(window);
 		glfwSwapInterval(1); // Enable vsync
 		gl3wInit();
-		return 0;
+		return true;
 	}
+	return false;
 }
 
 int main(int, char**)
-{
-	if (glfw_setup()) return 1;
-
-	MapGenUI *uiWindow = new MapGenUI(window);
-	GridTexture *gridWindowTex = new GridTexture(25, 25);
-
-	CellGrid* a = new CellGrid(4, 4); 
-	CellGrid* b = a->NextStep();
-    
+{ 
+	if (!glfwSetupWindow(1600, 900, "Cellular Automata Map Generator 152017")) return 1;
+	MapGenUI uiWindow = MapGenUI(window);
 	while (!glfwWindowShouldClose(window)) // Main loop
 	{
 		glfwPollEvents();
-		
-		uiWindow->Update();
-		uiWindow->UpdateCellsWindow( reinterpret_cast<void*>(gridWindowTex->Update()) );
-		uiWindow->Render();
-
-		// Now, based on ui settings, make changes to map texture
-		gridWindowTex->SetTexelColor(2, 3, GT_RED);
-		
+		{
+			uiWindow.Update();
+			uiWindow.Render();
+		}
         glfwSwapBuffers(window);
-    }
-
-    delete uiWindow;
-	delete gridWindowTex;
-
+    } 
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
